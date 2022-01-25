85ded24892df1d4f5ae45559501bcdaf99a6adfe
Duje Duša <duje.dusa@gmail.com>
2022-01-25 16:09:04 +0100

    Removed unused svgs, added modal with icon attributions.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import Contact from "./Contact";
 import Home from "./Home";
 import NavBar from "./NavBar";
 import "./style.css";
-import facebookIcon from "./assets/fb.svg";
-import linkedInIcon from "./assets/linkedin.svg";
-import emailIcon from "./assets/gmail.svg";
+import facebookIcon from "./assets/facebook-square-brands.svg";
+import linkedInIcon from "./assets/linkedin-brands.svg";
+import emailIcon from "./assets/envelope-solid.svg";
 import { HashLink } from "react-router-hash-link";
+import Attribution from "./Attribution";
+import { FunctionComponent, useState } from "react";
+import Modal from "./Modal";
 
-const App = () => {
+const App: FunctionComponent = () => {
+  const [showModal, updateShowModal] = useState(false);
+  const toggleModal = (): void => {
+    updateShowModal(!showModal);
+    console.log(showModal);
+  };
+
   return (
     <div className="page-container">
       <BrowserRouter>
@@ -63,6 +72,21 @@ const App = () => {
                 </div>
               </div>
             </div>
+            <span className="toggle-modal-text-btn" onClick={toggleModal}>
+              View attributions
+            </span>
+            {showModal ? (
+              <Modal>
+                <Attribution />
+                <span
+                  style={{ margin: "0" }}
+                  className="toggle-modal-text-btn"
+                  onClick={toggleModal}
+                >
+                  Close
+                </span>
+              </Modal>
+            ) : null}
             <div className="row">
               <div className="col">
                 <hr />
---
+++ src/Attribution.tsx
@@ -0,0 +1,276 @@
+import { FunctionComponent } from "react";
+
+const Attribution: FunctionComponent = () => {
+  return (
+    <div className="attributions">
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://commons.wikimedia.org/wiki/File:Visual_Studio_Icon_2019.svg"
+        >
+          Microsoft Visual Studio
+        </a>
+        , Public domain, via Wikimedia Commons
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://commons.wikimedia.org/wiki/File:Visual_Studio_Code_1.35_icon.svg"
+        >
+          Microsoft Visual Studio Code
+        </a>
+        , Public domain, via Wikimedia Commons
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://commons.wikimedia.org/wiki/File:.NET_Core_Logo.svg"
+        >
+          Jason Groce
+        </a>
+        , Public domain, via Wikimedia Commons
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/angular?style=brands"
+        >
+          Angular
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/react?style=brands"
+        >
+          React
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/html5?style=brands"
+        >
+          HTML5
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/css3-alt?style=brands"
+        >
+          CSS3
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/js-square?style=brands"
+        >
+          JavaScript
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/git-alt?style=brands"
+        >
+          Git
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/gitlab?style=brands"
+        >
+          GitLab
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/bitbucket?style=brands"
+        >
+          Bitbucket
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/facebook?style=brands"
+        >
+          Facebook
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/linkedin?style=brands"
+        >
+          Linkedin
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+      <br />
+      <span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://fontawesome.com/v5.15/icons/envelope?style=solid"
+        >
+          Envelope
+        </a>
+        <span>, licensed under </span>
+        <a
+          style={{ color: "#e0e0e0" }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by/4.0/"
+        >
+          CC BY 4.0
+        </a>
+        <span> / Changed original size and background color</span>
+      </span>
+    </div>
+  );
+};
+
+export default Attribution;
